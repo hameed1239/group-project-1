f805ca956878285389c3ab06c6bcbcfe2ab076d9
hameed kazeem <hameed.kazeem@gmail.com>
2020-09-14 02:53:02 -0400

    Removed all consoleLogs and commented codes

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -16,7 +16,6 @@ var currentWindSpeed = document.querySelector("#windspeed");
 var weatherAPIKEY = '7b0197c226ae249e6f7f72a52e0e15b5';
 var aerisAPIKEY = 'CMvFQF9jag1d3EMFHicMv';
 var aerisSecretKey = 'BQdV0ALmk1uCwHhrboePox3rLJ6dwF9TjP8pzV9n';
-
 $(".btn").on("click", function () {
     event.preventDefault();
     destination = $(".form-control").val();
@@ -27,89 +26,59 @@ $(".btn").on("click", function () {
         destination = "NEWYORK CITY";
     }
     if (states.includes(destination)) {
-        $("#my-modal-body").html(`${destination} is not a valid city!`);
+        destination = destination.toUpperCase();
+        $("#my-modal-body").html(`${destination} is Not a Valid City!`);
         $('#myModal').modal('show');
     }
     else {
         getGeocode();
     }
 });// get input, store in var destination, and call getGeocode
-
-
 function changeDestinationName(destination) {
     $("#city").html(`${destination} LOCAL GUIDE`);
 }// update destination name
-
 function getGeocode() {
     if (!destination) {
-        console.log(`no destination entered`);
+        $("#my-modal-body").html(`Enter a US City`);
+        $('#myModal').modal('show');
         return;
     }
     else {
         fetch(`https://cors-anywhere.herokuapp.com/https://api.opentripmap.com/0.1/en/places/geoname?name=${destination}&apikey=5ae2e3f221c38a28845f05b60fec7edd6c3842c3f334aa9d51ab3bfd`)
             .then(function (response) {
-                //console.log(response.json());
                 return response.json();
             })
             .then(function (response) {
                 if (response.status === "NOT_FOUND") {
-                    //console.log(response.json());
-                    console.log("Not Found");
+                    $("#my-modal-body").html(`City is Not Valid`);
+                    $('#myModal').modal('show');
+                    return;
                 }
                 else {
-                    console.log(response);
                     //const places = getPlaces(response);
                     destination = response.name;
                     storeDestination();
                     getPlacesXID(response);
                     lat = response.lat;
                     lon = response.lon;
-                    destinationCoord = `${response.lat},${response.lon}`
-                    
+                    destinationCoord = `${response.lat},${response.lon}`;
                 }
             })
     }
-
-
 }// fetch destination Geocode, update Lat and call 
-
 function getPlacesXID(response) {
-    //     debugger;
-    //const response = getGeocode(destination);
-    // if (response === "Not Found") {
-    //     //handle with modal or error <p>
-    //     console.log("Not Found");
-    //     return ("Not Found");
-    // }
-    // else {
-    console.log(response);
     fetch(`https://cors-anywhere.herokuapp.com/https://api.opentripmap.com/0.1/en/places/radius?radius=16093.4&lon=${response.lon}&lat=${response.lat}&rate=3&kinds=other%2Cnatural%2Csport%2Cindustrial_facilities%2Chistorical_places%2Carchaeology%2Ccultural%2Carchitecture%2Camusements&limit=6&apikey=5ae2e3f221c38a28845f05b60fec7edd6c3842c3f334aa9d51ab3bfd`)
         .then(function (response) {
-            console.log("reached 2nd call to json()")
             return response.json();
         })
         .then(function (response) {
-            console.log("reached conversion")
-            console.log(response);
             getPlaceInfo(response.features);
         })
-
-    // }
-}
-
-
-// }
+}// fetch POIs within a 5 miles radius
 function getPlaceInfo(xIDs) {
-    //const response = getGeocode(destination);
-    // const data = response;
-    // console.log(data);
-
     const promises = xIDs.map((xIDs) => {
         return fetch(`https://api.opentripmap.com/0.1/en/places/xid/${xIDs.properties.xid}?apikey=5ae2e3f221c38a28845f05b60fec7edd6c3842c3f334aa9d51ab3bfd`)
     })
-
-   // console.log(promises.json())
-
     Promise.all(promises)
         .then(values => {
             return Promise.all(values.map(function (value) {
@@ -117,33 +86,25 @@ function getPlaceInfo(xIDs) {
             }))
         })
         .then(function (response) {
-            console.log(response);
-            $(".carousel-inner").empty();
-            response.map(function (response, index) {
-                var imgSrc = response.preview.source;
-                var pxIndex = imgSrc.lastIndexOf("px");
-                var newImgSrc = imgSrc.replace(`${imgSrc[pxIndex - 3]}${imgSrc[pxIndex - 2]}${imgSrc[pxIndex - 1]}`, "999");
-                //console.log(newImgSrc);
-                // imgSrcArray.push(newImgSrc);
-               var carouselItem = $(`<div class="carousel-item text-center"><img src=${newImgSrc}  class="mx-auto d-block" alt=...><div class="carousel-caption d-none d-md-block"><h2>${response.name}</h2><p>${response.wikipedia_extracts.text}</p></div></div>`)
-                
-                $(".carousel-inner").append(carouselItem);
-                console.log(index);
-                console.log(carouselItem);
-                if (index === 0) {
-
-                    $(".carousel-item").addClass("active");
-                }
-
-            });
-            fetchAirports();
-            weatherFetch();
+            displayCarousel(response);
+        });  
+    }// fetch images and place description
+    function displayCarousel(response) {
+        $(".carousel-inner").empty();
+        response.map(function (response, index) {
+            var imgSrc = response.preview.source;
+            var pxIndex = imgSrc.lastIndexOf("px");
+            var newImgSrc = imgSrc.replace(`${imgSrc[pxIndex - 3]}${imgSrc[pxIndex - 2]}${imgSrc[pxIndex - 1]}`, "999");
+            var carouselItem = $(`<div class="carousel-item text-center"><img src=${newImgSrc}  class="mx-auto d-block" alt=...><div class="carousel-caption d-none d-md-block"><h2>${response.name}</h2><p>${response.wikipedia_extracts.text}</p></div></div>`)
+            $(".carousel-inner").append(carouselItem);
+            if (index === 0) {
+                $(".carousel-item").addClass("active");
+            }
         });
-
-   
-}
+        fetchAirports();
+        weatherFetch();
+}// display carousel
 function weatherFetch() {
-
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${weatherAPIKEY}`)
         .then(function (response) {
             return response.json();
@@ -159,42 +120,30 @@ function weatherFetch() {
                     addUVIndex(response);
                 })
         });
-    // fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityValue}&units=imperial&appid=${APIKEY}`)
-    //     .then(function (response) {
-    //         return response.json();
-    //     })
-    //     .then(function (response) {
-    //         displayForcast(response);
-    //     });
-}
+}// fetch weather information
 function displayWeather(response) {
-    // var formatedDate = currentDate.format("M/D/YYYY");
-    // currentDay.innerHTML = `${cityValue} ${formatedDate}`;
     var iconSrc = `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
     weatherIcon.setAttribute("src", iconSrc);
     airportIcon.setAttribute("src", "./assets/images/airport-clipart.jpg");
     currentTemp.innerHTML = `Temperature: ${response.main.temp} &#8457`;
     currentHumidity.innerHTML = `Humidity: ${response.main.humidity}%`;
     currentWindSpeed.innerHTML = `Windspeed: ${response.wind.speed} MPH`
-
-}
+}// display weather information
 function addUVIndex(response) {
     currentUVIndex.innerHTML = `UV Index: ${response.value}`;
-}
-
+}// display UV index
 function fetchAirports() {
     fetch(`https://api.aerisapi.com/places/airports/closest/?client_id=${aerisAPIKEY}&client_secret=${aerisSecretKey}&p=${lat},${lon}&limit=1&radius=500miles&filter=largeairport&`)
         .then(function (response) {
             return response.json();
         })
         .then(function (response) {
-            console.log(response);
             $("#airport-code").html((response.response[0].profile.iata).toUpperCase());
             $("#airport").html(response.response[0].place.name);
             fullDestination = `${response.response[0].place.city}, ${response.response[0].place.state}`;
             changeDestinationName(fullDestination);
     })
-}
+}// fetch closest airport
 
 function storeDestination() {
     if (typeof (Storage) !== "undefined") {
@@ -206,7 +155,7 @@ function storeDestination() {
         window.alert("Sorry, your browser does not support Web Storage...");
     }
 
-}//store city value in local storage
+}//store destination value in local storage
 
 function onLoad() {
     if (localStorage.getItem("destination") !== null) {
@@ -216,5 +165,5 @@ function onLoad() {
         destination = "Cleveland";
     }
     getGeocode(destination);
-}
+}// functions to call onLoad
 onLoad();
